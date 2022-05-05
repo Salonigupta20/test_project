bb23ed80bd5bf3eb2eee892ac0457596c411426a
“rakeshgsharma” <srakesh95@gmail.com>
2022-05-05 15:40:31 +0530

    add route protection to dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,14 +1,21 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { Context as AuthContext } from '../context/auth-context';
 
 export default function() {
-    const { logout } = useContext(AuthContext);
+    const { state, logout } = useContext(AuthContext);
     const navigate = useNavigate();
     const handleLogout = () => {
         logout();
         navigate("/sign-in");
     }
+
+    useEffect(() => {
+        if (!state.isLoggedIn) {
+            navigate('/sign-in')
+        }
+    }, [state.isLoggedIn])
+
     return (
     <>
         <div>User Successfully Signed In </div>
